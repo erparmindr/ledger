28583daeec8c2af4f37041159b2f0a6332b4dcc3
erparmindr <erparmindr@users.noreply.github.com>
2026-07-26 23:43:07 -0400

    fix: add cashback/rebate/reward to refund keywords for credit card imports

--- js/services/import-preview.js
+++ js/services/import-preview.js
@@ -115,7 +115,7 @@ window.Ledger.openImportPreviewModal = function(parsedRows, preselectedAccount, 
     return '<option value="'+a.id+'" '+(a.id===preselectedAccount?'selected':'')+'>'+window.Ledger.escapeHtml(a.name)+'</option>';
   }).join("");
 
-  var REFUND_KW = /\b(refund|return|reversal|chargeback|credit\s*refund)\b/i;
+  var REFUND_KW = /\b(refund|return|reversal|chargeback|cashback|rebate|reward|credit\s*refund)\b/i;
 
   parsedRows.forEach(function(r){
     if(r.suggestedCategoryId === undefined){
